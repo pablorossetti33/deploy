512ced0387a95354a59ab5ad5899c33e5b64a327
Pablo Rossetti <prensap.ross@gmail.com>
2023-04-02 15:18:34 -0300

    fix

--- api/index.js
+++ api/index.js
@@ -20,16 +20,16 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {getApiInfo} = require("./src/utils/countries.utils")
+require("dotenv").config();
 
 getApiInfo();
 
 
-
-
-
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at', process.env.PORT); // eslint-disable-line no-console
   });
 });
+
+
